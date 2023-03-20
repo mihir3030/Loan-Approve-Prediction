d1fed01a6fcc1ced4545c601553f6cb0af5c0b1a
preet333 <preetpatel3398@gmail.com>
2023-03-20 20:56:07 +0530

    data ingestion staged added to pipeline

---
+++ src/LoanApproval/pipeline/training_pipeline.py
@@ -0,0 +1,20 @@
+from LoanApproval.data_ingestion import DataIngestionConfigurationManager, DataIngestion
+from LoanApproval import log
+
+#STAGE_NAME = "Data Ingestion Stage"
+
+def start_training_pipeline():
+    config = DataIngestionConfigurationManager()
+    data_ingestion_config = config.data_ingestion_config()
+    data_ingestion = DataIngestion(config = data_ingestion_config)
+    data_ingestion.download_data()
+    data_ingestion.save_data()
+
+if __name__ == "__main__":
+    try:
+        log.info(f">>>>>>>>>>>>>>>>> Training Pipeline started")
+        start_training_pipeline()
+        log.info(f">>>>>>>>>>>>>>>>>> Training Pipeline compleated successfully\n\n X========================X\n\n")
+    except Exception as e:
+        log.exception(e)
+        raise e
